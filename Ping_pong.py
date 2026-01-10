7324a9178cc8da6e16f9923e807c87f53f718e21
Kirilant777 <kiriant.zk@gmail.com>
2026-01-10 09:27:48 +0300

    фиксация изменений

--- Ping_pong.py
+++ Ping_pong.py
@@ -3,6 +3,9 @@ from pygame import*
 w = 1600
 h = 900
 
+p1 = 0
+p2 = 0
+
 window = display.set_mode((w, h))
 display.set_caption("Пинг-Понг")
 background = transform.scale(image.load("pin-pong_fon.jpg"), (w, h))
@@ -11,6 +14,10 @@ clock = time.Clock()
 
 game = True
 finish = False
+
+font.init()
+font1 = font.Font(None, 100)
+font2 = font.Font(None, 170)
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_imaige, player_x, player_y, player_speed, player_w, player_h):
@@ -26,15 +33,15 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update1(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_w] and self.rect.y < 800:
+        if keys_pressed[K_s] and self.rect.y < 700:
             self.rect.y += self.speed
-        if keys_pressed[K_s] and self.rect.y > 0:
+        if keys_pressed[K_w] and self.rect.y > 0:
             self.rect.y -= self.speed
     def update2(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_d] and self.rect.y < 800:
+        if keys_pressed[K_DOWN] and self.rect.y < 700:
             self.rect.y += self.speed
-        if keys_pressed[K_a] and self.rect.y > 0:
+        if keys_pressed[K_UP] and self.rect.y > 0:
             self.rect.y -= self.speed
 
 class Ball(GameSprite):
@@ -43,17 +50,35 @@ class Ball(GameSprite):
         self.speed_x = player_speed
         self.speed_y = player_speed
     def update(self):
+        global p1, p2
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
-
+        if self.rect.y >= 800 or self.rect.y <= 0:
+            self.speed_y *= -1
+        if sprite.collide_rect(player1, self) or sprite.collide_rect(player2, self):
+            self.speed_x *= -1
+        if self.rect.x <= 0:
+            self.rect.x = 730
+            self.rect.y = 400
+            self.speed_x *= -1
+            p2 += 1
+        if self.rect.x >= 1500:
+            self.rect.x = 730
+            self.rect.y = 400
+            self.speed_x *= -1
+            p1 += 1
 
 player1 = Player("platform.png", 100, 350, 20, 30, 200)
 player2 = Player("platform.png", 1470, 350, 20, 30, 200)
 
-ball = Ball("ball.png", 750, 400, 20, 100, 100)
+ball = Ball("ball.png", 730, 400, 20, 100, 100)
 
 while game:
     window.blit(background, (0, 0))
+    text_score1 = font1.render("Пропущено: " + str(p1), 1, (255, 255, 255))
+    window.blit(text_score1, (250, 10))
+    text_score2 = font1.render("Пропущено: " + str(p2), 1, (255, 255, 255))
+    window.blit(text_score2, (850, 10))
     for e in event.get():
         if e.type == QUIT:
             game = False
@@ -65,5 +90,16 @@ while game:
         player1.reset()
         player2.reset()
         ball.reset()
+
+    if p1 >= 5:
+        text_score2 = font2.render("Победа левого игрока" , 1, (255, 50, 50))
+        window.blit(text_score2, (170, 390))
+        finish = True
+
+    if p2 >= 5:
+        text_score2 = font2.render("Победа правого игрока" , 1, (255, 50, 50))
+        window.blit(text_score2, (170, 390))
+        finish = True
+
     display.update()
     clock.tick(60)
